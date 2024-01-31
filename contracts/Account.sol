2eaefd33f6c293342bc9ef7e0a39f9b0e7be68dd
Gabriel Temtsen <gabrieltemtsen@gmail.com>
2024-01-31 16:02:53 +0100

    deployed smartAccount

--- contracts/Account.sol
+++ contracts/Account.sol
@@ -7,8 +7,24 @@ import "@account-abstraction/contracts/interfaces/IAccount.sol";
 
 
 contract Account is IAccount {
+    uint public count;
+    address public owner;
 
-     function validateUserOp(UserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds) external returns (uint256 validationData) {
+    constructor(address _owner) {
+        owner = _owner;
+    }
+
+     function validateUserOp(UserOperation calldata , bytes32 , uint256 ) external pure returns (uint256 validationData) {
         return 0;
      }
+
+     function execute() external {
+         count++;
+     }
+}
+
+contract AccountFactory {
+    function createAccount(address owner) external returns (address) {
+        return address(new Account(owner));
+    }
 }
--- scripts/test.js
+++ scripts/test.js
@@ -1,12 +1,16 @@
  
  const hre = require("hardhat");
  const EP_ADDR = "0x5fbdb2315678afecb367f032d93f642f64180aa3"
-
+const ACCOUNT_ADDR = "0xCafac3dD18aC6c6e92c921884f9E4176737C052c"
 async function main() {
 
-    const code =await hre.ethers.provider.getCode(EP_ADDR)
+  const account = await hre.ethers.getContractAt("Account", ACCOUNT_ADDR);
+  const count = await account.count();
+  console.log(count)
 
-console.log(code)
+//     const code =await hre.ethers.provider.getCode(EP_ADDR)
+
+// console.log(code)
 
 }
 
